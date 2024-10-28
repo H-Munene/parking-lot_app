0d2634a5a1725cc2315e416077711ddf1075da90
Hezekiah Munene <hezemunene01@gmail.com>
2024-10-28 10:49:22 +0300

    created register fn in  AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,11 +11,24 @@ class AuthController extends Controller
     //
     public function register(Request $request) {
         $request->validate([
-            // 'username' => ,
-            // 'vehicle_lp'=>,
-            // 'email'=>,
-            // 'phone_number'=>,
+            'username' => 'required|string|max:50',
+            'vehicle_lp'=>'required|string|max:7',
+            'email'=> 'required|email',
+            'phone_number'=> 'required|integer|unique',
+            'password'=> 'required|string|max:8'
         ]);
+
+        $user = User::firstorNew(
+            ['username' => $request->username],
+            ['vehicle_lp' => $request->vehicle_lp],
+            ['email'=>$request->email],
+            ['phone_number'=>$request->phone_number],
+            ['password'=>$request->password],
+        );
+
+        $user->save();
+
+        return $user;
     }
 
     public function login() {
