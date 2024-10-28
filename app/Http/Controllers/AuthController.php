914069c045f25655660e9b5bb51f7a4aa1c2bf4e
Hezekiah Munene <hezemunene01@gmail.com>
2024-10-28 10:19:35 +0300

    created auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request) {
+        
+    }
+
+    public function login() {
+
+    }
+}
